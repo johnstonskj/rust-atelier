54ffa28e4f596860dc770c0939f8cdf127692e3d
Simon Johnston <johnstonskj@gmail.com>
2020-06-22 11:51:03 -0700

    Completed more README and library documentation.

--- atelier-core/src/lib.rs
+++ atelier-core/src/lib.rs
@@ -1,5 +1,81 @@
 /*!
-Rust native core model for the AWS Smithy IDL.
+This crate provides a Rust native core model for the AWS [Smithy](https://github.com/awslabs/smithy) Interface Definition Language.
+
+This crate is the foundation for the Atelier set of crates, and provides the following components:
+
+1. The model structures themselves that represents a Smithy model, this is intended to be the
+   in-memory representation shared by all Atelier crates and tools.
+1. The model builder structures that allow for a fluent and easy construction of a core model.
+1. The prelude model containing the set of shapes defined in the Smithy specification.
+1. Traits for reading/writing models in different representations.
+1. Trait and simple implementation for a model registry.
+1. A common `error` module to be used by all Atelier crates.
+
+# Example
+
+The following example demonstrates the builder interface to create a model for a simple service. The
+service, `MessageOfTheDay` has a single resource `Message`. The resource has an identifier for the
+date, but the `read` operation does not make the date member required and so will return the message
+for the current date.
+
+```rust
+use atelier_core::error::ErrorSource;
+use atelier_core::model::builder::values::{ArrayBuilder, ObjectBuilder};
+use atelier_core::model::builder::{
+    Builder, ListBuilder, MemberBuilder, ModelBuilder, OperationBuilder, ResourceBuilder,
+    ServiceBuilder, SimpleShapeBuilder, StructureBuilder, TraitBuilder,
+};
+use atelier_core::model::{Identifier, Model, ShapeID};
+
+let model = ModelBuilder::new("example.motd")
+    .shape(
+        ServiceBuilder::new("MessageOfTheDay")
+            .documentation("Provides a Message of the day.")
+             .version("2020-06-21")
+            .resource("Message")
+            .build(),
+    )
+    .shape(
+        ResourceBuilder::new("Message")
+            .identifier("date", "Date")
+            .read("GetMessage")
+            .build(),
+    )
+    .shape(
+        SimpleShapeBuilder::string("Date")
+            .add_trait(TraitBuilder::pattern(r"^\d\d\d\d\-\d\d-\d\d$").build())
+            .build(),
+    )
+    .shape(
+        OperationBuilder::new("GetMessage")
+            .readonly()
+            .input("GetMessageInput")
+            .output("GetMessageOutput")
+            .error("BadDateValue")
+            .build(),
+    )
+    .shape(
+        StructureBuilder::new("GetMessageInput")
+            .add_member(
+                MemberBuilder::new("date")
+                    .refers_to("Date")
+                    .build(),
+            )
+            .build(),
+    )
+    .shape(
+        StructureBuilder::new("GetMessageOutput")
+            .add_member(MemberBuilder::string("message").required().build())
+            .build(),
+    )
+    .shape(
+        StructureBuilder::new("BadDateValue")
+            .error(ErrorSource::Client)
+            .add_member(MemberBuilder::string("errorMessage").required().build())
+            .build(),
+    )
+    .build();
+```
 */
 
 #![warn(
