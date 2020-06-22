527654deb943d4e5e2435cf40889165408ab66e3
Simon Johnston <johnstonskj@gmail.com>
2020-06-22 10:00:29 -0700

    Added macro for string constants

--- atelier-json/src/syntax.rs
+++ atelier-json/src/syntax.rs
@@ -1,39 +1,50 @@
 // ------------------------------------------------------------------------------------------------
+// Macros
+// ------------------------------------------------------------------------------------------------
+
+#[doc(hidden)]
+macro_rules! string_const {
+    ($name:ident, $value:expr) => {
+        pub(crate) const $name: &str = $value;
+    };
+}
+
+// ------------------------------------------------------------------------------------------------
 // JSON Key Names
 // ------------------------------------------------------------------------------------------------
 
-pub(crate) const K_COLLECTION_OPERATIONS: &str = "collectionOperations";
-pub(crate) const K_CREATE: &str = "create";
-pub(crate) const K_DELETE: &str = "delete";
-pub(crate) const K_ERRORS: &str = "errors";
-pub(crate) const K_IDENTIFIERS: &str = "identifiers";
-pub(crate) const K_INPUT: &str = "input";
-pub(crate) const K_LIST: &str = "list";
-pub(crate) const K_MEMBER: &str = "member";
-pub(crate) const K_METADATA: &str = "metadata";
-pub(crate) const K_MEMBERS: &str = "members";
-pub(crate) const K_OPERATIONS: &str = "operations";
-pub(crate) const K_OUTPUT: &str = "output";
-pub(crate) const K_PUT: &str = "put";
-pub(crate) const K_READ: &str = "read";
-pub(crate) const K_RESOURCES: &str = "resources";
-pub(crate) const K_SHAPES: &str = "shapes";
-pub(crate) const K_SMITHY: &str = "smithy";
-pub(crate) const K_TARGET: &str = "target";
-pub(crate) const K_TRAITS: &str = "traits";
-pub(crate) const K_TYPE: &str = "type";
-pub(crate) const K_UPDATE: &str = "update";
-pub(crate) const K_VERSION: &str = "version";
+string_const!(K_COLLECTION_OPERATIONS, "collectionOperations");
+string_const!(K_CREATE, "create");
+string_const!(K_DELETE, "delete");
+string_const!(K_ERRORS, "errors");
+string_const!(K_IDENTIFIERS, "identifiers");
+string_const!(K_INPUT, "input");
+string_const!(K_LIST, "list");
+string_const!(K_MEMBER, "member");
+string_const!(K_METADATA, "metadata");
+string_const!(K_MEMBERS, "members");
+string_const!(K_OPERATIONS, "operations");
+string_const!(K_OUTPUT, "output");
+string_const!(K_PUT, "put");
+string_const!(K_READ, "read");
+string_const!(K_RESOURCES, "resources");
+string_const!(K_SHAPES, "shapes");
+string_const!(K_SMITHY, "smithy");
+string_const!(K_TARGET, "target");
+string_const!(K_TRAITS, "traits");
+string_const!(K_TYPE, "type");
+string_const!(K_UPDATE, "update");
+string_const!(K_VERSION, "version");
 
 // ------------------------------------------------------------------------------------------------
 // JSON Value Strings
 // ------------------------------------------------------------------------------------------------
 
-pub(crate) const V_LIST: &str = "list";
-pub(crate) const V_SET: &str = "set";
-pub(crate) const V_MAP: &str = "map";
-pub(crate) const V_STRUCTURE: &str = "structure";
-pub(crate) const V_UNION: &str = "union";
-pub(crate) const V_SERVICE: &str = "service";
-pub(crate) const V_OPERATION: &str = "operation";
-pub(crate) const V_RESOURCE: &str = "resource";
+string_const!(V_LIST, "list");
+string_const!(V_SET, "set");
+string_const!(V_MAP, "map");
+string_const!(V_STRUCTURE, "structure");
+string_const!(V_UNION, "union");
+string_const!(V_SERVICE, "service");
+string_const!(V_OPERATION, "operation");
+string_const!(V_RESOURCE, "resource");
