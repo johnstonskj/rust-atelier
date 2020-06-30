ae183dd379cff5113632d48b7a32b986defd47a7
Simon Johnston <johnstonskj@gmail.com>
2020-06-30 14:09:05 -0700

    Renamed test driver for easier reporting. Also output all defined shapes on successful parse.

---
+++ atelier-smithy/tests/smithy_success_tests.rs
@@ -0,0 +1,78 @@
+use atelier_core::io::read_model_from_string;
+use atelier_core::model::shapes::ShapeBody;
+use atelier_core::model::{Annotated, Named, ShapeID};
+use atelier_core::syntax::{
+    SHAPE_APPLY, SHAPE_LIST, SHAPE_MAP, SHAPE_OPERATION, SHAPE_RESOURCE, SHAPE_SERVICE, SHAPE_SET,
+    SHAPE_STRUCTURE, SHAPE_UNION,
+};
+use atelier_smithy::io::SmithyReader;
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+// ------------------------------------------------------------------------------------------------
+// Test Cases
+// ------------------------------------------------------------------------------------------------
+
+#[test]
+fn test_weather_example() {
+    test_file_parses("weather");
+}
+
+#[test]
+fn test_smithy_prelude() {
+    test_file_parses("prelude-traits");
+}
+
+// ------------------------------------------------------------------------------------------------
+// Private Functions
+// ------------------------------------------------------------------------------------------------
+
+const MANIFEST_DIR: &str = env!("CARGO_MANIFEST_DIR");
+
+fn test_file_parses(file_name: &str) {
+    let mut path = PathBuf::from_str(MANIFEST_DIR).unwrap();
+    path.push(format!("tests/good/{}.smithy", file_name));
+    println!("{:?}", path);
+    let mut file = File::open(path).unwrap();
+    let mut content: Vec<u8> = Vec::default();
+    let _ = file.read_to_end(&mut content).unwrap();
+
+    let mut reader = SmithyReader::default();
+    let result = read_model_from_string(&mut reader, content);
+    let trait_trait = ShapeID::from_str("trait").unwrap();
+    match result {
+        Ok(parsed) => {
+            let mut names = parsed
+                .shapes()
+                .map(|shape| {
+                    format!(
+                        "{:<32} -> {}{}",
+                        shape.id(),
+                        if shape.has_trait(&trait_trait) {
+                            "trait "
+                        } else {
+                            ""
+                        },
+                        match shape.body() {
+                            ShapeBody::SimpleType(v) => v.to_string(),
+                            ShapeBody::List(_) => SHAPE_LIST.to_string(),
+                            ShapeBody::Set(_) => SHAPE_SET.to_string(),
+                            ShapeBody::Map(_) => SHAPE_MAP.to_string(),
+                            ShapeBody::Structure(_) => SHAPE_STRUCTURE.to_string(),
+                            ShapeBody::Union(_) => SHAPE_UNION.to_string(),
+                            ShapeBody::Service(_) => SHAPE_SERVICE.to_string(),
+                            ShapeBody::Operation(_) => SHAPE_OPERATION.to_string(),
+                            ShapeBody::Resource(_) => SHAPE_RESOURCE.to_string(),
+                            ShapeBody::Apply => SHAPE_APPLY.to_string(),
+                        }
+                    )
+                })
+                .collect::<Vec<String>>();
+            names.sort();
+            print!("{:#?}", names)
+        }
+        Err(err) => panic!(err.to_string()),
+    }
+}
