d80e5d5c12d2ad1688e7e0f5dc8ed4afd1aff43a
Simon Johnston <johnstonskj@gmail.com>
2020-08-14 17:25:16 -0700

    Added a `MutableModelVisitor` trait.

--- atelier-core/src/model/visitor.rs
+++ atelier-core/src/model/visitor.rs
@@ -17,6 +17,29 @@ use crate::model::{Model, ShapeID};
 // ------------------------------------------------------------------------------------------------
 
 macro_rules! visit_fn {
+    (mut $fn_name:ident, $shape_type:ty, $doc:expr) => {
+        #[doc = $doc]
+        #[allow(unused_variables)]
+        fn $fn_name(
+            &mut self,
+            id: &ShapeID,
+            traits: &[AppliedTrait],
+            shape: &$shape_type,
+        ) -> Result<(), Self::Error> {
+            Ok(())
+        }
+    };
+    (mut $fn_name:ident, $doc:expr) => {
+        #[doc = $doc]
+        #[allow(unused_variables)]
+        fn $fn_name(
+            &mut self,
+            id: &ShapeID,
+            traits: &[AppliedTrait],
+        ) -> Result<(), Self::Error> {
+            Ok(())
+        }
+    };
     ($fn_name:ident, $shape_type:ty, $doc:expr) => {
         #[doc = $doc]
         #[allow(unused_variables)]
@@ -74,6 +97,31 @@ pub trait ModelVisitor {
     visit_fn! { unresolved_id, "Called for each `ShapeKind::Unresolved` shape identifier in this model's **shapes** collection." }
 }
 
+///
+/// Identical to [ModelVisitor](trait.ModelVisitor.html), but `self` is a mutable reference.
+///
+pub trait MutableModelVisitor {
+    /// The error which will be returned by this model.
+    type Error;
+
+    /// Called once for each key in the model's metadata.
+    #[allow(unused_variables)]
+    fn metadata(&self, key: &str, value: &Value) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    visit_fn! { mut simple_shape, Simple, "Called for each `ShapeKind::Simple` in this model's **shapes** collection." }
+    visit_fn! { mut list, ListOrSet, "Called for each `ShapeKind::List` in this model's **shapes** collection." }
+    visit_fn! { mut set, ListOrSet, "Called for each `ShapeKind::Set` in this model's **shapes** collection." }
+    visit_fn! { mut map, Map, "Called for each `ShapeKind::Map` in this model's **shapes** collection." }
+    visit_fn! { mut structure, StructureOrUnion, "Called for each `ShapeKind::Structure` in this model's **shapes** collection." }
+    visit_fn! { mut union, StructureOrUnion, "Called for each `ShapeKind::Union` in this model's **shapes** collection." }
+    visit_fn! { mut service, Service, "Called for each `ShapeKind::Service` in this model's **shapes** collection." }
+    visit_fn! { mut operation, Operation, "Called for each `ShapeKind::Operation` in this model's **shapes** collection." }
+    visit_fn! { mut resource, Resource, "Called for each `ShapeKind::Resource` in this model's **shapes** collection." }
+    visit_fn! { mut unresolved_id, "Called for each `ShapeKind::Unresolved` shape identifier in this model's **shapes** collection." }
+}
+
 // ------------------------------------------------------------------------------------------------
 // Public Functions
 // ------------------------------------------------------------------------------------------------
@@ -86,6 +134,35 @@ pub trait ModelVisitor {
 pub fn walk_model<V>(model: &Model, visitor: &V) -> Result<(), V::Error>
 where
     V: ModelVisitor,
+{
+    for (key, value) in model.metadata() {
+        visitor.metadata(key, value)?;
+    }
+
+    for shape in model.shapes() {
+        match &shape.body() {
+            ShapeKind::Simple(body) => visitor.simple_shape(shape.id(), &shape.traits(), &body)?,
+            ShapeKind::List(body) => visitor.list(shape.id(), &shape.traits(), &body)?,
+            ShapeKind::Set(body) => visitor.set(shape.id(), &shape.traits(), &body)?,
+            ShapeKind::Map(body) => visitor.map(shape.id(), &shape.traits(), &body)?,
+            ShapeKind::Structure(body) => visitor.structure(shape.id(), &shape.traits(), &body)?,
+            ShapeKind::Union(body) => visitor.union(shape.id(), &shape.traits(), &body)?,
+            ShapeKind::Service(body) => visitor.service(shape.id(), &shape.traits(), &body)?,
+            ShapeKind::Operation(body) => visitor.operation(shape.id(), &shape.traits(), &body)?,
+            ShapeKind::Resource(body) => visitor.resource(shape.id(), &shape.traits(), &body)?,
+            ShapeKind::Unresolved => visitor.unresolved_id(shape.id(), &shape.traits())?,
+        }
+    }
+
+    Ok(())
+}
+
+///
+/// Identical to [walk_model](fn.walk_model.html), but `visitor` is a mutable reference.
+///
+pub fn walk_model_mut<V>(model: &Model, visitor: &mut V) -> Result<(), V::Error>
+where
+    V: MutableModelVisitor,
 {
     for (key, value) in model.metadata() {
         visitor.metadata(key, value)?;
