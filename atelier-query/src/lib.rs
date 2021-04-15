062394c39172fddd0080f62ed4a1da88b1244dd2
Simon Johnston <johnstonskj@gmail.com>
2021-04-15 12:05:03 -0700

    Code Format

--- atelier-query/src/lib.rs
+++ atelier-query/src/lib.rs
@@ -8,23 +8,23 @@ More detailed description, with
 */
 
 #![warn(
-// ---------- Stylistic
-future_incompatible,
-nonstandard_style,
-rust_2018_idioms,
-trivial_casts,
-trivial_numeric_casts,
-// ---------- Public
-missing_debug_implementations,
-missing_docs,
-unreachable_pub,
-// ---------- Unsafe
-unsafe_code,
-// ---------- Unused
-unused_extern_crates,
-unused_import_braces,
-unused_qualifications,
-unused_results,
+    // ---------- Stylistic
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_idioms,
+    trivial_casts,
+    trivial_numeric_casts,
+    // ---------- Public
+    missing_debug_implementations,
+    missing_docs,
+    unreachable_pub,
+    // ---------- Unsafe
+    unsafe_code,
+    // ---------- Unused
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results,
 )]
 
 #[macro_use]
