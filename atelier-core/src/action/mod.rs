7ff1759eee52edcf1ce6c0e3154c721b80a08f29
Simon Johnston <johnstonskj@gmail.com>
2021-06-27 20:47:44 -0700

    Documentation tidy.

--- atelier-core/src/action/mod.rs
+++ atelier-core/src/action/mod.rs
@@ -1,91 +1,91 @@
 /*!
-* This module provides a set of traits that describes actions that can operate on models. These
-* actions take three major forms; linters, validators, and transformers.
-*
-* 1. **Linters**; these inspect the model for stylistic issues, they are a subset of validators.
-* 1. **Validators**; these inspect models for errors and warnings that may produce errors when the
-*    model is used.
-* 1. **Transformers**; these take in a model and transform it into another model.
-*
-* # Example
-*
-* The following example is taken from the Smithy specification discussing
-* [relative name resolution](https://awslabs.github.io/smithy/1.0/spec/core/shapes.html#relative-shape-id-resolution).
-* The `run_validation_actions` function is commonly used to take a list of actions to be performed
-* on the model in sequence.
-*
-* ```rust
-* use atelier_core::action::validate::{
-*     run_validation_actions, CorrectTypeReferences
-* };
-* use atelier_core::action::Validator;
-* use atelier_core::builder::{
-*     ModelBuilder, ShapeTraits, SimpleShapeBuilder, StructureBuilder, TraitBuilder
-* };
-* use atelier_core::model::Model;
-* use atelier_core::Version;
-* use std::convert::TryInto;
-*
-* let model: Model = ModelBuilder::new(Version::V10, "smithy.example")
-*     .uses("foo.baz#Bar")
-*     .structure(
-*         StructureBuilder::new("MyStructure")
-*             .member("a", "MyString")
-*             .member("b", "smithy.example#MyString")
-*             .member("d", "foo.baz#Bar")
-*             .member("f", "String")
-*             .member("g", "MyBoolean")
-*             .apply_trait(TraitBuilder::new("documentation"))
-*             .into(),
-*     )
-*     .simple_shape(SimpleShapeBuilder::string("MyString"))
-*     .simple_shape(SimpleShapeBuilder::boolean("MyBoolean"))
-*     .try_into().unwrap();
-* let result = run_validation_actions(&mut [
-*         Box::new(CorrectTypeReferences::default()),
-*     ], &model, false);
-* ```
-*
-* This will result in the following list of validation errors. Note that the error is denoted against
-* shape or member identifier accordingly.
-*
-* ```text
-* [
-*     ActionIssue {
-*         reporter: "CorrectTypeReferences",
-*         level: Info,
-*         message: "The simple shape (smithy.example#MyBoolean) is simply a synonym, did you mean to add any constraint traits?",
-*         locus: Some(
-*             ShapeID {
-*                 namespace: NamespaceID(
-*                     "smithy.example",
-*                 ),
-*                 shape_name: Identifier(
-*                     "MyBoolean",
-*                 ),
-*                 member_name: None,
-*             },
-*         ),
-*     },
-*     ActionIssue {
-*         reporter: "CorrectTypeReferences",
-*         level: Info,
-*         message: "The simple shape (smithy.example#MyString) is simply a synonym, did you mean to add any constraint traits?",
-*         locus: Some(
-*             ShapeID {
-*                 namespace: NamespaceID(
-*                     "smithy.example",
-*                 ),
-*                 shape_name: Identifier(
-*                     "MyString",
-*                 ),
-*                 member_name: None,
-*             },
-*         ),
-*     },
-* ]
-* ```
-*
+This module provides a set of traits that describes actions that can operate on models. These
+actions take three major forms; linters, validators, and transformers.
+
+1. **Linters**; these inspect the model for stylistic issues, they are a subset of validators.
+1. **Validators**; these inspect models for errors and warnings that may produce errors when the
+   model is used.
+1. **Transformers**; these take in a model and transform it into another model.
+
+# Example
+
+The following example is taken from the Smithy specification discussing
+[relative name resolution](https://awslabs.github.io/smithy/1.0/spec/core/shapes.html#relative-shape-id-resolution).
+The `run_validation_actions` function is commonly used to take a list of actions to be performed
+on the model in sequence.
+
+```rust
+use atelier_core::action::validate::{
+    run_validation_actions, CorrectTypeReferences
+};
+use atelier_core::action::Validator;
+use atelier_core::builder::{
+    ModelBuilder, ShapeTraits, SimpleShapeBuilder, StructureBuilder, TraitBuilder
+};
+use atelier_core::model::Model;
+use atelier_core::Version;
+use std::convert::TryInto;
+
+let model: Model = ModelBuilder::new(Version::V10, "smithy.example")
+    .uses("foo.baz#Bar")
+    .structure(
+        StructureBuilder::new("MyStructure")
+            .member("a", "MyString")
+            .member("b", "smithy.example#MyString")
+            .member("d", "foo.baz#Bar")
+            .member("f", "String")
+            .member("g", "MyBoolean")
+            .apply_trait(TraitBuilder::new("documentation"))
+            .into(),
+    )
+    .simple_shape(SimpleShapeBuilder::string("MyString"))
+    .simple_shape(SimpleShapeBuilder::boolean("MyBoolean"))
+    .try_into().unwrap();
+let result = run_validation_actions(&mut [
+        Box::new(CorrectTypeReferences::default()),
+    ], &model, false);
+```
+
+This will result in the following list of validation errors. Note that the error is denoted against
+shape or member identifier accordingly.
+
+```text
+[
+    ActionIssue {
+        reporter: "CorrectTypeReferences",
+        level: Info,
+        message: "The simple shape (smithy.example#MyBoolean) is simply a synonym, did you mean to add any constraint traits?",
+        locus: Some(
+            ShapeID {
+                namespace: NamespaceID(
+                    "smithy.example",
+                ),
+                shape_name: Identifier(
+                    "MyBoolean",
+                ),
+                member_name: None,
+            },
+        ),
+    },
+    ActionIssue {
+        reporter: "CorrectTypeReferences",
+        level: Info,
+        message: "The simple shape (smithy.example#MyString) is simply a synonym, did you mean to add any constraint traits?",
+        locus: Some(
+            ShapeID {
+                namespace: NamespaceID(
+                    "smithy.example",
+                ),
+                shape_name: Identifier(
+                    "MyString",
+                ),
+                member_name: None,
+            },
+        ),
+    },
+]
+```
+
 */
 
 use crate::error::Result as ModelResult;
