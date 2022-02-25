1ad59cd6ec23498196bc0fb67a2b18902f10f9be
Brendan McKee <brendan@stedi.com>
2022-02-24 22:20:47 -0800

    test helper compares actual OpenApi spec

--- atelier-openapi/tests/describe_openapi_writer.rs
+++ atelier-openapi/tests/describe_openapi_writer.rs
@@ -1,14 +1,12 @@
-use atelier_core::io::{read_model_from_file, write_model_to_string, ModelWriter};
+use atelier_core::io::{read_model_from_file, write_model_to_string};
 use atelier_openapi::OpenApiWriter;
 use atelier_smithy::SmithyReader;
-use atelier_test::parse_and_compare_to_files;
-use std::{
-    fs,
-    path::{Path, PathBuf},
-};
+use std::{fs, path::PathBuf};
+
+use okapi::openapi3;
 
 #[test]
-fn test_smithy_prelude() {
+fn test_unions() {
     test("union-test");
 }
 
@@ -16,38 +14,25 @@ const MANIFEST_DIR: &str = env!("CARGO_MANIFEST_DIR");
 
 // test helper which reads a smithy file, converts it to openapi, and compares with the
 // corresponding openapi spec from the /models directory.
-// just a naive line by line equality check for now until we have a real model to work with
 fn test(file_name: &str) {
     let source_file = PathBuf::from(format!(
         "{}/tests/models/{}.smithy",
         MANIFEST_DIR, file_name
     ));
-    let expected_file = PathBuf::from(format!(
+    let expected_file_path = PathBuf::from(format!(
         "{}/tests/models/{}.openapi.jsoN",
         MANIFEST_DIR, file_name
     ));
 
     let mut reader = SmithyReader::default();
-    let model = read_model_from_file(&mut reader, PathBuf::from(source_file)).unwrap();
+    let model = read_model_from_file(&mut reader, source_file).unwrap();
 
     let mut writer = OpenApiWriter::default();
-    let output = write_model_to_string(&mut writer, &model).unwrap();
+    let actual_str = write_model_to_string(&mut writer, &model).unwrap();
+    let actual_spec: openapi3::OpenApi = serde_json::from_str(&actual_str).unwrap();
 
-    let expected_lines: Vec<String> = fs::read_to_string(expected_file)
-        .unwrap()
-        .split('\n')
-        // .split(LINE_ENDING)
-        .map(|s| {
-            format!(
-                "{:?}",
-                s.replace("\\n", "\n")
-                    .replace("\\t", "\t")
-                    .replace("\\\"", "\"")
-            )
-        })
-        .collect();
+    let expected_file = fs::read_to_string(expected_file_path).unwrap();
+    let expected_spec: openapi3::OpenApi = serde_json::from_str(&expected_file).unwrap();
 
-    let actual_lines: Vec<&str> = output.split("\n").collect();
-
-    assert_eq!(actual_lines, expected_lines);
+    assert_eq!(actual_spec, expected_spec);
 }
